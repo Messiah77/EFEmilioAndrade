c3ac10785e1ab98139f23e4e23838a8a7550cbb6
José Emilio Andrade Izquierdo <jose.andrade.izquierdo@udla.edu.ec>
2023-02-01 09:11:11 -0500

    Creación tabla satelite BD

--- EFEmilioAndrade/JEAIModels/Satelite.cs
+++ EFEmilioAndrade/JEAIModels/Satelite.cs
@@ -3,10 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SQLite;
 
 
 namespace EFEmilioAndrade.JEAIModels
 {
+    [Table("satelite")]
+
     public class Satelite
     {
         public string name { get; set; }
