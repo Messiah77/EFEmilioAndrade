68969cb5611d40ffa93148a6939dffcf081d1c23
José Emilio Andrade Izquierdo <jose.andrade.izquierdo@udla.edu.ec>
2023-02-01 08:37:03 -0500

    definición del code behind de la clase satelite para que muestre los datos recuperados de la API cuando esto sea solicitado

--- EFEmilioAndrade/MainPage.xaml.cs
+++ EFEmilioAndrade/MainPage.xaml.cs
@@ -1,14 +1,32 @@
-﻿namespace EFEmilioAndrade;
+﻿using EFEmilioAndrade.JEAIModels;
+using Newtonsoft.Json;
+using System.Net;
+
+namespace EFEmilioAndrade;
 
 public partial class MainPage : ContentPage
 {
-	int count = 0;
 
 	public MainPage()
 	{
 		InitializeComponent();
 	}
 
+    public async void Button_Clicked(object sender, EventArgs e)
+    {
+        var request = new HttpRequestMessage();
+        request.RequestUri = new Uri("https://api.wheretheiss.at/v1/satellites");
+        request.Method = HttpMethod.Get;
+        request.Headers.Add("Accept", "application/json");
+        var client = new HttpClient();
+        HttpResponseMessage response = await client.SendAsync(request);
 
+        if (response.StatusCode == HttpStatusCode.OK)
+        {
+            String content = await response.Content.ReadAsStringAsync();
+            var resultado = JsonConvert.DeserializeObject<List<Satelite>>(content);
+            ListaDemo.ItemsSource = resultado;
+        }
+    }
 }
 
